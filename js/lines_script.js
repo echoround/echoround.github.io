72da9cb59fd12c5f94b3d792eec49340cbef49b3
unknown <hansvilu@protonmail.com>
2021-12-13 07:22:30 +0200

    update

--- js/lines_script.js
+++ js/lines_script.js
@@ -21,11 +21,9 @@ var NUM_PARTICLES = ( ( ROWS = 100 ) * ( COLS = 300 ) ),
     particle,
     canvas,
     mouse,
-    stats,
     list,
     ctx,
     tog,
-    man,
     dx, dy,
     mx, my,
     d, t, f,
@@ -49,7 +47,6 @@ function init() {
   canvas = document.createElement('canvas');
 
   ctx = canvas.getContext( '2d' );
-  man = false;
   tog = true;
 
   list = [];
@@ -74,29 +71,19 @@ function init() {
     bounds = container.getBoundingClientRect();
     mx = e.clientX - bounds.left;
     my = e.clientY - bounds.top;
-    man = true;
 
   });
 
-  if ( typeof Stats === 'function' ) {
-    document.body.appendChild( ( stats = new Stats() ).domElement );
-  }
+
 
   container.appendChild( canvas );
 }
 
 function step() {
 
-  if ( stats ) stats.begin();
+
 
   if ( tog = !tog ) {
-
-    // if ( !man ) {
-    //
-    //   t = +new Date() * 0.001;
-    //   mx = w * 0.5 + ( Math.cos( t * 2.1 ) * Math.cos( t * 0.9 ) * w * 0.45 );
-    //   my = h * 0.5 + ( Math.sin( t * 3.2 ) * Math.tan( Math.sin( t * 0.8 ) ) * h * 0.45 );
-    // }
 
     for ( i = 0; i < NUM_PARTICLES; i++ ) {
 
@@ -129,7 +116,7 @@ function step() {
     ctx.putImageData( a, 0, 0 );
   }
 
-  if ( stats ) stats.end();
+
 
   requestAnimationFrame( step );
 }
